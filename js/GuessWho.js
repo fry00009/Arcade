a7c050df59662b6d6f1cc4fe7eafa4267e579785
fry00009 <98852473+fry00009@users.noreply.github.com>
2023-10-21 10:11:26 -0500

    Add files via upload

---
+++ js/GuessWho.js
@@ -0,0 +1,209 @@
+$(document).ready(function() {
+
+ var titles = [
+  'Akaza',
+  'Anya',
+  'Asta',
+  'Asuna',
+  'Ban',
+  'Benimaru',
+  'Denji',
+  'Goku',
+  'Gon',
+  'Killua',
+  'Kirishima',
+  'Kirito',
+  'Lloyd',
+  'Luffy',
+  'Marin',
+  'Mirio',
+  'Power',
+  'Saitama',
+  'Shion',
+  'Tanjiro',
+  'Trunks',
+  'Tsuyu',
+  'Vanessa',
+  'Yor'
+ ]
+
+  var n = Math.floor(Math.random() * titles.length);
+
+   $(".Character").html(titles[n]);
+
+    $(".1").click(function() {
+      $(".1").addClass("hide");
+    });
+    $(".1").dblclick(function() {
+      $(".1").removeClass("hide");
+    });
+    
+    $(".2").click(function() {
+      $(".2").addClass("hide");
+    });
+    $(".2").dblclick(function() {
+      $(".2").removeClass("hide");
+    });
+
+    $(".3").click(function() {
+      $(".3").addClass("hide");
+    });
+    $(".3").dblclick(function() {
+      $(".3").removeClass("hide");
+    });
+
+    $(".4").click(function() {
+      $(".4").addClass("hide");
+    });
+    $(".4").dblclick(function() {
+      $(".4").removeClass("hide");
+    });
+
+    $(".5").click(function() {
+      $(".5").addClass("hide");
+    });
+    $(".5").dblclick(function() {
+      $(".5").removeClass("hide");
+    });
+
+    $(".6").click(function() {
+      $(".6").addClass("hide");
+    });
+    $(".6").dblclick(function() {
+      $(".6").removeClass("hide");
+    });
+
+    $(".7").click(function() {
+      $(".7").addClass("hide");
+    });
+    $(".7").dblclick(function() {
+      $(".7").removeClass("hide");
+    });
+
+    $(".8").click(function() {
+      $(".8").addClass("hide");
+    });
+    $(".8").dblclick(function() {
+      $(".8").removeClass("hide");
+    });
+
+    $(".9").click(function() {
+      $(".9").addClass("hide");
+    });
+    $(".9").dblclick(function() {
+      $(".9").removeClass("hide");
+    });
+    
+    $(".10").click(function() {
+      $(".10").addClass("hide");
+    });
+    $(".10").dblclick(function() {
+      $(".10").removeClass("hide");
+    });
+
+    $(".11").click(function() {
+      $(".11").addClass("hide");
+    });
+    $(".11").dblclick(function() {
+      $(".11").removeClass("hide");
+    });
+
+    $(".12").click(function() {
+      $(".12").addClass("hide");
+    });
+    $(".12").dblclick(function() {
+      $(".12").removeClass("hide");
+    });
+
+    $(".13").click(function() {
+      $(".13").addClass("hide");
+    });
+    $(".13").dblclick(function() {
+      $(".13").removeClass("hide");
+    });
+
+    $(".14").click(function() {
+      $(".14").addClass("hide");
+    });
+    $(".14").dblclick(function() {
+      $(".14").removeClass("hide");
+    });
+
+    $(".15").click(function() {
+      $(".15").addClass("hide");
+    });
+    $(".15").dblclick(function() {
+      $(".15").removeClass("hide");
+    });
+
+    $(".16").click(function() {
+      $(".16").addClass("hide");
+    });
+    $(".16").dblclick(function() {
+      $(".16").removeClass("hide");
+    });
+
+    $(".17").click(function() {
+      $(".17").addClass("hide");
+    });
+    $(".17").dblclick(function() {
+      $(".17").removeClass("hide");
+    });
+
+    $(".18").click(function() {
+      $(".18").addClass("hide");
+    });
+    $(".18").dblclick(function() {
+      $(".18").removeClass("hide");
+    });
+
+    $(".19").click(function() {
+      $(".19").addClass("hide");
+    });
+    $(".19").dblclick(function() {
+      $(".19").removeClass("hide");
+    });
+
+    $(".20").click(function() {
+      $(".20").addClass("hide");
+    });
+    $(".20").dblclick(function() {
+      $(".20").removeClass("hide");
+    });
+
+    $(".21").click(function() {
+      $(".21").addClass("hide");
+    });
+    $(".21").dblclick(function() {
+      $(".21").removeClass("hide");
+    });
+
+    $(".22").click(function() {
+      $(".22").addClass("hide");
+    });
+    $(".22").dblclick(function() {
+      $(".22").removeClass("hide");
+    });
+
+    $(".23").click(function() {
+      $(".23").addClass("hide");
+    });
+    $(".23").dblclick(function() {
+      $(".23").removeClass("hide");
+    });
+
+    $(".24").click(function() {
+      $(".24").addClass("hide");
+    });
+    $(".24").dblclick(function() {
+      $(".24").removeClass("hide");
+    });
+
+    $(".25").click(function() {
+      $(".25").addClass("hide");
+    });
+    $(".25").dblclick(function() {
+      $(".25").removeClass("hide");
+    });
+
+});
